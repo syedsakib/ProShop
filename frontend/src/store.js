6c2854fa2ad868b76e1c91cd7eab5786f02a155a
syedsakib <sakibsyed01@gmail.com>
2020-10-20 11:32:56 +0600

    checkout done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,12 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers';
-import { orderCreateReducer } from './reducers/orderReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderListMyReducer,
+} from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducers,
@@ -25,6 +30,9 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 });
 
 const cartItemFromStorage = localStorage.getItem('cartItems')
